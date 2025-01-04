7a65e6422c9654083ecc7ba953907449bf877093
Akshay <akshay77shetgar@gmail.com>
2025-01-04 19:07:19 +0530

    changed region

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -1,5 +1,5 @@
 const { SESClient } = require("@aws-sdk/client-ses");
-const REGION = "us-east-1";
+const REGION = "eu-north-1";
 const sesClient = new SESClient({ region: REGION });
 
 module.exports = { sesClient };
